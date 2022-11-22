db1e6c9337fd43e8aeb6e316bc006967d898be9d
Sargis Abrahamyan <118532124+Sargis-Abrahamyan@users.noreply.github.com>
2022-11-22 11:45:44 +0400

    Update TodosButton.jsx

--- src/Components/shared/genericButton/TodosButton.jsx
+++ src/Components/shared/genericButton/TodosButton.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 import styles from "./scss/todoBtnStyle.module.scss";
 
 export const TodosButton = ({ color, content, click }) => {
-    return <button className={styles.btnStyle} style={{ color }} onClick={click}>{content}</button>
+    return <button className={styles.btnStyle} style={{color}} onClick={click}>{content}</button>
 };
 
